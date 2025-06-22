dab98c71ca3347070bc0e508c4eb5881a827ef38
gha3mi <36126354+gha3mi@users.noreply.github.com>
2025-06-22 20:25:53 +0000

    Deploying to gh-pages-ford from @ gha3mi/setup-fortran-conda@47b4b9243355cd6d82614e5fcd29b19f9233cad0 🚀

--- src/check.f90
+++ src/check.f90
@@ -1,6 +1,11 @@
 program check
    use, intrinsic :: iso_fortran_env
    use main
+   use fortime
    implicit none
+   type(timer) :: t
+
+   call t%timer_start()
    call say_hello()
+   call t%timer_stop()
 end program

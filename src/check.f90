6fc37a5498d9f69c198b376609e5cbb51f5fee93
gha3mi <36126354+gha3mi@users.noreply.github.com>
2025-06-24 09:37:37 +0000

    Deploying to gh-pages-ford from @ gha3mi/setup-fortran-conda@f5ca5e10188b339e4e27ee1d9685e9f1e13f10bf 🚀

--- src/check.f90
+++ src/check.f90
@@ -1,5 +1,9 @@
 program check
+   use, intrinsic :: iso_fortran_env, only: real64
    use main
    implicit none
+   real(real64) :: x
    call say_hello()
+   x = 1.0_real64
+   print *, "x = ", x
 end program

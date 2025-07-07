04ea348680e75b156f830b60717ed14ed0452fdb
gha3mi <36126354+gha3mi@users.noreply.github.com>
2025-07-07 19:49:59 +0000

    Deploy FORD docs

--- doc/ford/src/mpi_hello.f90
+++ doc/ford/src/mpi_hello.f90
@@ -1,4 +1,5 @@
 program mpi_hello
+#if defined(USE_MPI)
    use mpi_f08
    implicit none
    integer :: ierr, rank, size
@@ -7,4 +8,5 @@ program mpi_hello
    call mpi_comm_size(mpi_comm_world, size, ierr)
    print *, 'hello from process', rank, 'of', size
    call mpi_finalize(ierr)
+#endif
 end program

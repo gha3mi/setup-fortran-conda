3665c72c30002cbf364d90eb71cb3d388ee08eb6
Seyed Ali Ghasemi <info@gha3mi.com>
2026-02-26 18:37:25 +0100

    refactor: move STATUS.md generation to separate scripts

---
+++ scripts/generate-status-meson.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "Generating STATUS.md for test_meson..."
+badge_line=""
+
+mapfile -t job_lines < <(
+    curl -s -H "Authorization: token $GH_TOKEN" \
+    https://api.github.com/repos/$REPO/actions/runs/$RUN_ID/jobs?per_page=100 \
+    | jq -r '.jobs[] | select((.name | type == "string") and (.name | test(".*_meson$"))) | [.name, .conclusion] | @tsv'
+)
+
+for line in "${job_lines[@]}"; do
+    name="${line%%$'\t'*}"
+    conclusion="${line#*$'\t'}"
+
+    os="${name%%_*}"
+    compiler="${name#*_}"
+    key="${os}_${compiler}"
+
+    safe_key="${key//-/'--'}"
+
+    if [[ "$conclusion" == "success" ]]; then
+    color="brightgreen"
+    label="passing"
+    elif [[ "$conclusion" == "failure" ]]; then
+    color="red"
+    label="failing"
+    elif [[ "$conclusion" == "cancelled" ]]; then
+    color="lightgrey"
+    label="cancelled"
+    else
+    color="lightgrey"
+    label="pending"
+    fi
+
+    badge="![${key}](https://img.shields.io/badge/${safe_key}-${label}-${color})"
+    badge_line="$badge_line $badge"
+done
+
+echo "$badge_line" > STATUS.md
+mkdir -p status-meson
+mv STATUS.md status-meson/STATUS.md
+cat status-meson/STATUS.md

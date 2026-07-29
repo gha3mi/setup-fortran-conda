754c51709be11cd79d7faf55169e79b4f5fa84a7
gha3mi <36126354+gha3mi@users.noreply.github.com>
2026-07-29 11:52:48 +0000

    Deploy FORD docs

--- doc/ford/src/mpi_hello.F90
+++ doc/ford/src/mpi_hello.F90
@@ -3,10 +3,12 @@ program mpi_hello
    use mpi_f08
    implicit none
    integer :: ierr, rank, size
+
    call mpi_init(ierr)
    call mpi_comm_rank(mpi_comm_world, rank, ierr)
    call mpi_comm_size(mpi_comm_world, size, ierr)
-   print *, 'hello from process', rank, 'of', size
+   write(*, '(A,I0,A,I0)') 'MPI_RANK=', rank, ' MPI_SIZE=', size
+   if (size /= 2 .or. rank < 0 .or. rank >= size) error stop 1
    call mpi_finalize(ierr)
 #else
    print *, 'MPI not enabled'
